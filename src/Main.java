1974e3d4c4e85a07a34f8c112ea65b9ef9991443
Serg <ksyssau@ya.ru>
2023-04-17 22:35:02 +0300

    3 Задача

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,8 @@ public class Main {
     public static void main(String[] args) {
 
         task1();
-         task2();
+        task2();
+        task3();
     }
 
     public static void task1() {
@@ -14,11 +15,19 @@ public class Main {
         String fullName = lastName + " " + firstName + " " + middleName;
         System.out.println("ФИО сотрудника - " + fullName);
     }
+
     public static void task2() {
         String firstName = "Ivan";
         String middleName = "Ivanovich";
         String lastName = "Ivanov";
         String fullName = lastName + " " + firstName + " " + middleName;
         System.out.println("Данные ФИО сотрудника для заполнения отчета - " + fullName.toUpperCase());
+    }
+
+    public static void task3() {
+        String fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника - " + fullName);
+
     }
 }
