e47c9f4523f2bb5bb811ca62b9ead4aeb661da85
Serg <ksyssau@ya.ru>
2023-04-17 22:24:17 +0300

    2 Задача

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         task1();
-        // task2();
+         task2();
     }
 
     public static void task1() {
@@ -14,5 +14,11 @@ public class Main {
         String fullName = lastName + " " + firstName + " " + middleName;
         System.out.println("ФИО сотрудника - " + fullName);
     }
-
+    public static void task2() {
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + fullName.toUpperCase());
+    }
 }
